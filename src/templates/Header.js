3cf07cfaefc4b0ac60449561cc9623620457fa10
Diego Camino <diego.camino.r@gmail.com>
2020-04-25 22:57:00 -0500

    Adición íconos redes sociales

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,14 +3,20 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">
-          Rick&Morty
+          <a href="/spa-javascript-vanilla/#">
+            <img class="rickandmorty-logo" src="../../public/images/rickandmorty-logo.png">
           </a>
         </h1>
       </div>
       <div class="Header-nav">
-        <a href="#/about/">
+        <!--<a href="#/about/">
           About
+        </a>-->
+        <a target="_blank" href="https://github.com/diegocaminor/spa-javascript-vanilla">
+          <img class="social-icon" src="../../public/images/github-icon.svg" alt="GitHub">
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/diegocaminor/">
+          <img class="social-icon" src="../../public/images/linkedin-icon.svg" alt="LinkIn">
         </a>
       </div>
     </div>
